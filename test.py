347e8fd1cd0845bcbe0241bac3d6f86aefeb52f9
prl43 <prestonrobertlee@gmail.com>
2024-02-21 12:44:25 -0700

    Update test.py

--- test.py
+++ test.py
@@ -12,40 +12,43 @@
 # import libraries
 from math import sqrt
 
-# initialize variables
-file_path  = 'coding_qual_input.txt'
-max_number_segment = 0
-word_pair = {}
-word_pair[-1] = '[Word Not Found]'
+def decode(message_file):
+    # initialize variables
+    file_path  = 'coding_qual_input.txt'
+    max_number_segment = 0
+    word_pair = {}
+    word_pair[-1] = '[Word Not Found]'
+    output_message = []
 
-# loop over text file
-with open(file_path, 'r') as file:
-    for line in file:
-        # split the line into number and word
-        number_str, word = line.strip().split()
-        
-        # convert number part to an integer
-        number = int(number_str)
+    # loop over text file
+    with open(file_path, 'r') as file:
+        for line in file:
+            # split the line into number and word
+            number_str, word = line.strip().split()
+            
+            # convert number part to an integer
+            number = int(number_str)
 
-        # assign value:word in dictionary if an end of segment number
-        is_end_of_segment = False
-        is_end_of_segment = ( ( sqrt(8 * number + 1) - 1 ) % 2 == 0 )
+            # assign value:word in dictionary if an end of segment number
+            is_end_of_segment = False
+            is_end_of_segment = ( ( sqrt(8 * number + 1) - 1 ) % 2 == 0 )
 
-        if is_end_of_segment:
-            # store max number segment
-            number_segment = int(( sqrt(8 * number + 1) - 1 ) / 2)
-            if max_number_segment < number_segment:
-                max_number_segment = number_segment
-        
-            word_pair[number_segment] = word
+            if is_end_of_segment:
+                # store max number segment
+                number_segment = int(( sqrt(8 * number + 1) - 1 ) / 2)
+                if max_number_segment < number_segment:
+                    max_number_segment = number_segment
+            
+                word_pair[number_segment] = word
 
-## for loop from 1 to max number's segment 
-for number_segment in range(1, max_number_segment):
-    if number_segment in word_pair:
-        print( word_pair[number_segment], end=' ' )
-    else:
-        # print word pair not found
-        print( word_pair[-1], end=' ' )
+    ## for loop from 1 to max number's segment 
+    for number_segment in range(1, max_number_segment):
+        if number_segment in word_pair:
+            output_message.append( word_pair[number_segment] )
+        else:
+            # print word pair not found
+            output_message.append( word_pair[-1] )
+    
+    return ' '.join(output_message)
 
-
-
+print(decode('coding_qual_input.txt'))
